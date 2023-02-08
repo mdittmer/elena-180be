58dd69575f94353972c8f7d622de01140ea15500
Mark Dittmer <markdittmer@google.com>
2023-02-07 22:33:53 -0500

    Add more prayers and switch to px font sizes

--- scripts/prayers.mjs
+++ scripts/prayers.mjs
@@ -10,6 +10,70 @@ const prayers = [
     ],
     attribution: 'Bahá’u’lláh',
   },
+  {
+    paragraphs: [
+      'Remove not, O Lord, the festal board that hath been spread in Thy Name, and extinguish not the burning flame that hath been kindled by Thine unquenchable fire.  Withhold not from flowing that living water of Thine that murmureth with the melody of Thy glory and Thy remembrance, and deprive not Thy servants from the fragrance of Thy sweet savors breathing forth the perfume of Thy love.',
+      'Lord!  Turn the distressing cares of Thy holy ones into ease, their hardship into comfort, their abasement into glory, their sorrow into blissful joy, O Thou that holdest in Thy grasp the reins of all mankind!',
+      'Thou art, verily, the One, the Single, the Mighty, the All-Knowing, the All-Wise.',
+    ],
+    attribution: '‘Abdu’l-Bahá',
+  },
+  {
+    paragraphs: [
+      'O my Lord and my Hope!  Help Thou Thy loved ones to be steadfast in Thy mighty Covenant, to remain faithful to Thy manifest Cause, and to carry out the commandments Thou didst set down for them in Thy Book of Splendors; that they may become banners of guidance and lamps of the Company above, wellsprings of Thine infinite wisdom, and stars that lead aright, as they shine down from the supernal sky.',
+      'Verily, art Thou the Invincible, the Almighty, the All-Powerful.',
+    ],
+    attribution: '‘Abdu’l-Bahá',
+  },
+  {
+    paragraphs: [
+      'Glory be unto Thee, O Lord of the world and Desire of the nations, O Thou Who hast become manifest in the Greatest Name, whereby the pearls of wisdom and utterance have appeared from the shells of the great sea of Thy knowledge, and the heavens of divine revelation have been adorned with the light of the appearance of the sun of Thy countenance.',
+      'I beg of Thee, by that Word through which Thy proof was perfected among Thy creatures and Thy testimony was fulfilled amidst Thy servants, to strengthen Thy people in that whereby the face of the Cause will radiate in Thy dominion, the standards of Thy power will be planted among Thy servants, and the banners of Thy guidance will be raised throughout Thy dominions.',
+      'O my Lord!  Thou beholdest them clinging to the rope of Thy grace and holding fast unto the hem of the mantle of Thy beneficence.  Ordain for them that which may draw them nearer unto Thee, and withhold them from all else save Thee.',
+      'I beg of Thee, O Thou King of existence and Protector of the seen and the unseen, to make whosoever arises to serve Thy Cause as a sea moving by Thy desire, as one ablaze with the fire of Thy Sacred Tree, shining from the horizon of the heaven of Thy will.  Verily, Thou art the mighty One, Whom neither the power of all the world nor the strength of nations can weaken.  There is no God but Thee, the One, the Incomparable, the Protector, the Self-Subsistent.',
+    ],
+    attribution: 'Bahá’u’lláh',
+  },
+  {
+    paragraphs: [
+      'O God, my God!  Thou beholdest this weak one begging for celestial strength, this poor one craving Thy heavenly treasures, this thirsty one longing for the fountain of eternal life, this afflicted one yearning for Thy promised healing through Thy boundless mercy which Thou hast destined for Thy chosen servants in Thy kingdom on high.',
+      'O Lord!  I have no helper save Thee, no shelter besides Thee, and no sustainer except Thee.  Assist me with Thine angels to diffuse Thy holy fragrances and to spread abroad Thy teachings amongst the choicest of Thy people.',
+      'O my Lord!  Suffer me to be detached from aught else save Thee, to hold fast to the hem of Thy bounty, to be wholly devoted to Thy Faith, to remain fast and firm in Thy love and to observe what Thou hast prescribed in Thy Book.',
+      'Verily, Thou art the Powerful, the Mighty, the Omnipotent.',
+    ],
+    attribution: '‘Abdu’l-Bahá',
+  },
+  {
+    paragraphs: [
+      'I beseech Thee, O my God, by all the transcendent glory of Thy Name, to clothe Thy loved ones in the robe of justice and to illumine their beings with the light of trustworthiness.  Thou art the One that hath power to do as He pleaseth and Who holdeth within His grasp the reins of all things, visible and invisible.',
+    ],
+    attribution: 'Bahá’u’lláh',
+  },
+  {
+    paragraphs: [
+      'All-praise be to Thee, O Lord, my God!  I know not how to sing Thy praise, how to describe Thy glory, how to call upon Thy Name.  If I call upon Thee by Thy Name, the All-Possessing, I am compelled to recognize that He Who holdeth in His hand the immediate destinies of all created things is but a vassal dependent upon Thee, and is the creation of but a word proceeding from Thy mouth.  And if I proclaim Thee by the name of Him Who is the All-Compelling, I readily discover that He is but a suppliant fallen upon the dust, awe-stricken by Thy dreadful might, Thy sovereignty and power.  And if I attempt to describe Thee by glorifying the oneness of Thy Being, I soon realize that such a conception is but a notion which mine own fancy hath woven, and that Thou hast ever been immeasurably exalted above the vain imaginations which the hearts of men have devised.',
+      'The glory of Thy might beareth me witness!  Whoso claimeth to have known Thee hath, by virtue of such a claim, testified to his own ignorance; and whoso believeth himself to have attained unto Thee, all the atoms of the earth would attest his powerlessness and proclaim his failure.  Thou hast, however, by virtue of Thy mercy that hath surpassed the kingdoms of earth and heaven, deigned to accept from Thy servants the laud and honor they pay to Thine own exalted Self, and hast bidden them celebrate Thy glory, that the ensigns of Thy guidance may be unfurled in Thy cities and the tokens of Thy mercy be spread abroad among Thy nations, and that each and all may be enabled to attain unto that which Thou hast destined for them by Thy decree, and ordained unto them through Thine irrevocable will and purpose.',
+      'Having testified, therefore, unto mine own impotence and the impotence of Thy servants, I beseech Thee, by the brightness of the light of Thy beauty, not to refuse Thy creatures attainment to the shores of Thy most holy ocean.  Draw them, then, O my God, through the Divine sweetness of Thy melodies, towards the throne of Thy glory and the seat of Thine eternal holiness.  Thou art, verily, the Most Powerful, the Supreme Ruler, the Great Giver, the Most Exalted, the Ever-Desired.',
+      'Grant, then, O my God, that Thy servant who hath turned towards Thee, hath fixed his gaze upon Thee, and clung to the cord of Thy mercifulness and favor, may be enabled to partake of the living waters of Thy mercy and grace.  Cause him, then, to ascend unto the heights to which he aspireth, and withhold him not from that which Thou dost possess.  Thou art, verily, the Ever-Forgiving, the Most Bountiful.',
+    ],
+    attribution: 'Bahá’u’lláh',
+  },
+  {
+    paragraphs: [
+      'Lord!  Render victorious Thy forbearing servants in Thy days by granting them a befitting victory, inasmuch as they have sought martyrdom in Thy path.  Send down upon them that which will bring comfort to their minds, will rejoice their inner beings, will impart assurance to their hearts and tranquillity to their bodies and will enable their souls to ascend to the presence of God, the Most Exalted, and to attain the supreme Paradise and such retreats of glory as Thou hast destined for men of true knowledge and virtue.  Verily, Thou knowest all things, while we are but Thy servants, Thy thralls, Thy bondsmen and Thy poor ones.  No Lord but Thee do we invoke, O God our Lord, nor do we implore blessings or grace from anyone but Thee, O Thou Who art the God of mercy unto this world and the next.  We are but the embodiments of poverty, of nothingness, of helplessness and of perdition, while Thy whole Being betokeneth wealth, independence, glory, majesty and boundless grace.',
+      'Turn our recompense, O Lord, into that which well beseemeth Thee of the good of this world and of the next, and of the manifold bounties which extend from on high down to the earth below.',
+      'Verily, Thou art our Lord and the Lord of all things.  Into Thy hands do we surrender ourselves, yearning for the things that pertain unto Thee.',
+    ],
+    attribution: 'The Báb',
+  },
+  {
+    paragraphs: [
+      'He is the All-Glorious.',
+      'O my Lord, my King, my Ruler, and my Sovereign! I call upon Thee with my tongue, my heart, and my soul, saying: Clothe this servant of Thine with the robe of Thy care, the raiment of Thine unfailing help, and the armour of Thy protection. Assist him to make mention of Thee and to extol Thy virtues amidst Thy people, and unloose his tongue to utter Thy glorification and praise in every assemblage held to celebrate Thy unity and sanctity. Thou art, in truth, the Mighty, the Powerful, the All-Glorious, the Self-Subsisting.',
+    ],
+    attribution: '‘Abdu’l-Bahá',
+  },
+
 ];
 
 export async function getPrayerFromSeedArrayBuffer(seed) {
